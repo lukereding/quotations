2390cd36cefd6c6ff93c93d728195436b067dc98
luke reding <lukereding@gmail.com>
2021-02-27 09:39:54 -0500

    Add a couple quotes

--- make_categories.py
+++ make_categories.py
@@ -1,21 +1,22 @@
 import os
 import sys
+from typing import List
 
-if len(sys.argv) != 2 and not os.path.exists('./quotes.txt'):
+if len(sys.argv) != 2 and not os.path.exists("./quotes.txt"):
     sys.exit("pass a single argument: the path to the quotations file")
-elif os.path.exists('./quotes.txt'):
-    filename = './quotes.txt'
+elif os.path.exists("./quotes.txt"):
+    filename = "./quotes.txt"
 else:
     filename = sys.argv[1]
 
 if not os.path.exists(filename):
     sys.exit("Can't find {}".format(filename))
 
-## function to figure out all possible tags in the file
-def find_unique_tags(filename):
+
+def find_unique_tags(filename: str) -> List[str]:
     """Scan the input file and identify all unique categories."""
-    # list to store cateogories
-    categories = []
+
+    categories = set()
     with open(filename) as file:
         for line in file:
             all_matches = line.split(":::")
@@ -23,25 +24,29 @@ def find_unique_tags(filename):
             tags = [tag.strip() for tag in tags]
 
             for tag in tags:
-                if tag not in categories:
-                    categories.append(tag)
+                categories.add(tag)
+
     print("\nfound the following categories:\n")
     for cat in categories:
         print("  - {}".format(cat))
-    return(categories)
+
+    return list(categories)
+
 
 def write_to_file(quote, author, category):
     """Write a quote to a specific category file."""
-    filename = os.path.join('./quotes', '{}.txt'.format(category))
+
+    filename = os.path.join("./quotes", "{}.txt".format(category))
+
     if os.path.isfile(filename):
         file = open(filename, "a+")
     else:
         file = open(filename, "w+")
+
     file.write("{}\n- {}\n\n".format(quote, author))
     file.close()
 
 
-## creates list where each list is a different tag
 categories = find_unique_tags(filename)
 
 # new to make a new subdirectory to hold all the resulting files
--- quote_bkg.py
+++ quote_bkg.py
@@ -1,40 +1,46 @@
-import PIL
-from PIL import ImageFont, Image, ImageOps, ImageDraw
-import os
-import re
-from random import randint
 import textwrap
+from random import randint
 
+from PIL import Image, ImageDraw, ImageFont
 
+# point to the font to use
 font = ImageFont.truetype("/Users/lreding/Library/Fonts/NHaasGroteskTXPro-65Md.ttf", 90)
 
-cols = ['#5E5A6E', '#1F1B29', '#b33f62', '#8A0E38', '#CC503E', '#e39d25', '#C0392B', '#336E7B', '#16A085']
+cols = [
+    "#5E5A6E",
+    "#1F1B29",
+    "#b33f62",
+    "#8A0E38",
+    "#CC503E",
+    "#e39d25",
+    "#C0392B",
+    "#336E7B",
+    "#16A085",
+]
 
 width, height = 2650, 1600
 
-filename = "quotes.txt"
-
-counter = 0
-
-with open(filename, 'r') as file:
+with open("quotes.txt", "r") as file:
     for i, line in enumerate(file):
-        if counter < 30:
+        if i < 30:
             if line != "\n" and line.find(":::") != -1:
                 try:
                     # generate random col
-                    col = randint(0, len(cols)-1)
+                    col = randint(0, len(cols) - 1)
 
                     quote, name, *_ = line.split(":::")
 
                     quote_wrapped = textwrap.fill(quote.capitalize(), 50)
-                    quote_with_author = quote_wrapped + "\n\n" + name.strip()
+                    quote_with_author = (
+                        quote_wrapped + "\n\n" + name.strip().replace(".", "")
+                    )
 
                     img = Image.new("RGBA", (width, height), cols[col])
                     draw = ImageDraw.Draw(img)
 
-                    draw.text((10, 1000), quote_with_author, (255,255,255), font = font)
+                    draw.text((10, 1000), quote_with_author, (255, 255, 255), font=font)
                     draw = ImageDraw.Draw(img)
-                    img.save("{}.png".format(quote_wrapped[:10].replace(' ', '_')))
-                    counter += 1
-                except:
-                    print("problem with line {}".format(i))
+                    img.save("{}.png".format(quote_wrapped[:10].replace(" ", "_")))
+
+                except Exception as e:
+                    print(f"problem with line {i}: {e}")
